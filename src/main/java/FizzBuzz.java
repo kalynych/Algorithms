e4758379f96ff416f4605ce66ad42f7b3f08e9b8
kalynych <kalynych017@gmail.com>
2022-08-22 23:45:27 -0400

    Initial commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class FizzBuzz {
+
+    public String[] fizzBuzz (int startNumber,
+                              int endNumber) {
+        String[] numbers = new String[endNumber - startNumber + 1];
+        int newNumber = startNumber;
+
+        for (int i = startNumber - startNumber; i < (endNumber - startNumber + 1); i++) {
+            if (newNumber % (3 * 5) == 0) {
+                numbers[i] = "FizzBuzz";
+            } else if (newNumber % 3 == 0) {
+                numbers[i] = "Fizz";
+            } else if (newNumber % 5 == 0) {
+                numbers[i] = "Buzz";
+            } else {
+                numbers[i] = String.valueOf(newNumber);
+            }
+
+            newNumber += 1;
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
+        return numbers;
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testFizzBuzz() {
+        int startNumber = 1;
+        int endNumber = 30;
+        String[] expectedResult =
+                {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz"};
+
+        FizzBuzz fizzBuzzObj = new FizzBuzz();
+        String[] actualResult = fizzBuzzObj.fizzBuzz(startNumber, endNumber);
+
+        Assert.assertArrayEquals(expectedResult, actualResult);
+    }
+
+
+}
