25d07716546a71e0be532f1f474b3ea7050998be
kalynych <kalynych017@gmail.com>
2023-02-05 05:17:35 -0500

    format

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -8,13 +8,13 @@ public class FizzBuzzTest {
         int startNumber = 1;
         int endNumber = 30;
         String[] expectedResult =
-                {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz"};
+                {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz",
+                        "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29",
+                        "FizzBuzz"};
 
         FizzBuzz fizzBuzzObj = new FizzBuzz();
         String[] actualResult = fizzBuzzObj.fizzBuzz(startNumber, endNumber);
 
         Assert.assertArrayEquals(expectedResult, actualResult);
     }
-
-
 }
