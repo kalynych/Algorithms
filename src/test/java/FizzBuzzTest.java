d3c29b70340469f8d23916d8facc05ad9efe0f49
kalynych <kalynych017@gmail.com>
2023-03-09 23:23:59 -0500

    format_5

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,11 +6,11 @@ public class FizzBuzzTest {
     @Test
     public void testFizzBuzz() {
         int startNumber = 1;
-        int endNumber = 30;
+        int endNumber = 31;
         String[] expectedResult =
                 {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz",
                         "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29",
-                        "FizzBuzz"};
+                        "FizzBuzz", "31"};
 
         FizzBuzz fizzBuzzObj = new FizzBuzz();
         String[] actualResult = fizzBuzzObj.fizzBuzz(startNumber, endNumber);
